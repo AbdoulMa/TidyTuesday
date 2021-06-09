f079a10bd5420e1d368473b8633e68416228fd06
AbdoulMa <issamadjid1995@gmail.com>
2021-06-09 19:18:02 +0200

    Code indentation.

--- 2021_w24/tidytuesday_2021_w24.R
+++ 2021_w24/tidytuesday_2021_w24.R
@@ -38,7 +38,7 @@ lakes_points <- decade_summary %>% pmap_df(
 ) %>% 
   mutate(decade = rep(lakes_records$decade, lakes_records$proportion),# appropriate decade for each circle
          lake = rep(lakes_records$lake, lakes_records$proportion) # appropriate lake for each circle
-         )
+  )
 
 
 # Graphic -----------------------------------------------------------------
@@ -51,85 +51,85 @@ my_gpar <- gpar(
 
 # Plot
 (plot <-  lakes_points%>% 
-  ggplot(aes(x, y, fill = lake)) + 
-  geom_point(
-    size = 4,
-    pch = 21
-    
-  ) +
-   grid_panel(
-     grob = function(data, coords) {
-       if (data$PANEL[1] == 1) {
-         .x <- min(coords$x) + 0.085
-         .y <- min(coords$y) + 0.085
-         gList(
-           segmentsGrob(
-             .x, .y,
-             unit(.x/2, "npc"),
-             unit(.y/2, "npc"),
-             gp = gpar(
-               col = "black",
-               lwd = 0.5
-             )
-           ),
-           textGrob(
-             label = "Each  circle represents\n1% of fish collected.",
-             x = unit(.x/2.5, "npc"),
-             y = unit(.y/2.5, "npc"),
-             just = c("left", "top"),
-             gp = my_gpar
-           )
-         )
-       }
-       else if (data$PANEL[1] == 3) {
-         gList(
-           textGrob(
-             label = "In 1880s, approximatively 171,913 \n thousand pounds (71%) of fish\n were caught from Herie Lake.",
-             x = unit(.75, "npc"),
-             y = unit(1., "npc"),
-             just = c("left", "top"),
-             gp = my_gpar
-           )
-         )
-       }
-       else if (data$PANEL[1] == 6) {
-         gList(
-           textGrob(
-             label = "The Great Lakes Storm of 1913\n had severly impacted the Lake Huron fishing\n ecosystem and production.",
-             x = unit(.3, "npc"),
-             y = unit(1., "npc"),
-             just = c("right", "top"),
-             gp = my_gpar
-           )
-         )
-       }
+    ggplot(aes(x, y, fill = lake)) + 
+    geom_point(
+      size = 4,
+      pch = 21
       
-     else if (data$PANEL[1] == 11) {
-         gList(
-           textGrob(
-             label = "Dr. Howard Tanner, the  fisheries\nchief  for the state of Michigan\nre-introduced the salmon in 1966.",
-             x = unit(.3, "npc"),
-             y = unit(1., "npc"),
-             just = c("right", "top"),
-             gp = my_gpar
-           )
-         )
-       }
-       else if (data$PANEL[1] == 13) {
-         gList(
-           textGrob(
-             label = "The extinction of Blue Pike, approximatively in 1983\n impacted Lake Erie fishing industry but relatively little\n compared to the proportion of fish collected.",
-             x = unit(.5, "npc"),
-             y = unit(0., "npc"),
-             just = c("left", "top"),
-             gp = my_gpar
-           )
-         )
-       } else {
-         nullGrob()
-       }
-     }
-   )+ 
+    ) +
+    grid_panel(
+      grob = function(data, coords) {
+        if (data$PANEL[1] == 1) {
+          .x <- min(coords$x) + 0.085
+          .y <- min(coords$y) + 0.085
+          gList(
+            segmentsGrob(
+              .x, .y,
+              unit(.x/2, "npc"),
+              unit(.y/2, "npc"),
+              gp = gpar(
+                col = "black",
+                lwd = 0.5
+              )
+            ),
+            textGrob(
+              label = "Each  circle represents\n1% of fish collected.",
+              x = unit(.x/2.5, "npc"),
+              y = unit(.y/2.5, "npc"),
+              just = c("left", "top"),
+              gp = my_gpar
+            )
+          )
+        }
+        else if (data$PANEL[1] == 3) {
+          gList(
+            textGrob(
+              label = "In 1880s, approximatively 171,913 \n thousand pounds (71%) of fish\n were caught from Herie Lake.",
+              x = unit(.75, "npc"),
+              y = unit(1., "npc"),
+              just = c("left", "top"),
+              gp = my_gpar
+            )
+          )
+        }
+        else if (data$PANEL[1] == 6) {
+          gList(
+            textGrob(
+              label = "The Great Lakes Storm of 1913\n had severly impacted the Lake Huron fishing\n ecosystem and production.",
+              x = unit(.3, "npc"),
+              y = unit(1., "npc"),
+              just = c("right", "top"),
+              gp = my_gpar
+            )
+          )
+        }
+        
+        else if (data$PANEL[1] == 11) {
+          gList(
+            textGrob(
+              label = "Dr. Howard Tanner, the  fisheries\nchief  for the state of Michigan\nre-introduced the salmon in 1966.",
+              x = unit(.3, "npc"),
+              y = unit(1., "npc"),
+              just = c("right", "top"),
+              gp = my_gpar
+            )
+          )
+        }
+        else if (data$PANEL[1] == 13) {
+          gList(
+            textGrob(
+              label = "The extinction of Blue Pike, approximatively in 1983\n impacted Lake Erie fishing industry but relatively little\n compared to the proportion of fish collected.",
+              x = unit(.5, "npc"),
+              y = unit(0., "npc"),
+              just = c("left", "top"),
+              gp = my_gpar
+            )
+          )
+        } else {
+          nullGrob()
+        }
+      }
+    )+ 
     labs(
       title =  str_to_upper("Proportions of fish caught by lake through decades"),
       fill = "Lakes",
@@ -138,43 +138,43 @@ my_gpar <- gpar(
       x = NULL,
       y = NULL 
     ) + 
-   scale_x_continuous(
-     limits = c(-5,5)
-   ) + 
-   scale_y_continuous(
-     limits = c(-5,5)
-   ) + 
-   scale_fill_manual(
-     values = c("#E69F00","#009E73","#F0E442","#0072B2","#D55E00","#CC79A7") # colorblind_pal() 
-   )+ 
-   guides(fill=guide_legend(
-     label.position = 'bottom', 
-     title.position = 'top', 
-     keywidth=.55,
-     keyheight=.15,
-     default.unit="inch", 
-     title.hjust = .5,
-     title.vjust = 0,
-     label.vjust = 3,
-     nrow = 1)) + 
-  coord_equal( clip = "off") + 
-  facet_wrap(~decade) + 
-   theme_minimal(base_family = "Lato") +
-   theme (
-     panel.spacing  = unit( 1.1, unit = "cm"),
-   legend.spacing.x = unit(0, 'cm'),
- legend.title=element_text(family = "Lato Black",size = rel(1.3)),
- legend.text = element_text(size = rel(0.8)),
- legend.margin=margin(-10,0,-1,0),
- legend.position = 'bottom',
- legend.box.margin=margin(15,0,15,0),
- strip.text = element_text(family = "Lato Black", size = rel(1.1)),
- plot.title  = element_text(family = "Lato Black", size = rel(1.3), hjust = .5, margin = margin(t = 10, b= 10)),
- plot.caption = element_markdown(color = "grey35", size = rel(0.8)),
- strip.placement = "outside",
- axis.text = element_blank(),
- panel.grid = element_blank()
-)
+    scale_x_continuous(
+      limits = c(-5,5)
+    ) + 
+    scale_y_continuous(
+      limits = c(-5,5)
+    ) + 
+    scale_fill_manual(
+      values = c("#E69F00","#009E73","#F0E442","#0072B2","#D55E00","#CC79A7") # colorblind_pal() 
+    )+ 
+    guides(fill=guide_legend(
+      label.position = 'bottom', 
+      title.position = 'top', 
+      keywidth=.55,
+      keyheight=.15,
+      default.unit="inch", 
+      title.hjust = .5,
+      title.vjust = 0,
+      label.vjust = 3,
+      nrow = 1)) + 
+    coord_equal( clip = "off") + 
+    facet_wrap(~decade) + 
+    theme_minimal(base_family = "Lato") +
+    theme (
+      panel.spacing  = unit( 1.1, unit = "cm"),
+      legend.spacing.x = unit(0, 'cm'),
+      legend.title=element_text(family = "Lato Black",size = rel(1.3)),
+      legend.text = element_text(size = rel(0.8)),
+      legend.margin=margin(-10,0,-1,0),
+      legend.position = 'bottom',
+      legend.box.margin=margin(15,0,15,0),
+      strip.text = element_text(family = "Lato Black", size = rel(1.1)),
+      plot.title  = element_text(family = "Lato Black", size = rel(1.3), hjust = .5, margin = margin(t = 10, b= 10)),
+      plot.caption = element_markdown(color = "grey35", size = rel(0.8)),
+      strip.placement = "outside",
+      axis.text = element_blank(),
+      panel.grid = element_blank()
+    )
 )  
 
 # Saving ------------------------------------------------------------------
