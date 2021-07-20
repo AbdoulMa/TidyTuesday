6b6235c1653ca2d2483b645cec472713384d988a
AbdoulMa <issamadjid1995@gmail.com>
2021-07-20 21:10:29 +0200

    2021-W30 Final Submission.

--- 2021_w30/tidytuesday_2021_w30.R
+++ 2021_w30/tidytuesday_2021_w30.R
@@ -2,28 +2,17 @@
 library(tidyverse)
 library(ggnewscale)
 library(gggrid)
+library(ggtext)
 
 # Read data & Wrangling ---------------------------------------------------
 drought <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-20/drought.csv')
-states <- readr::read_csv("https://raw.githubusercontent.com/jasonong/List-of-US-States/master/states.csv")
+states <- readr::read_csv("https://raw.githubusercontent.com/jasonong/List-of-US-States/master/states.csv") %>% 
+  tibble::add_row(State = "Puerto Rico", Abbreviation = "PR")
 
-
-col_hot <- "#B8001F"
-col_hot_low <- "#FFBEC9"
-col_cold <- "#0067A3"
-col_cold_low <- "#BAE6FF"
-drought %>% 
-  filter(drought_lvl == "D3")
-
-drought %>% 
-  filter(state_abb == "CA") %>% 
-  group_by(map_date) %>% 
-  slice_max(order_by = area_pct,n = 1, with_ties = F) 
 
 monthly_drought <- drought %>% 
   mutate(map_date = lubridate::ymd(map_date),
-         drought_lvl = fct_other(drought_lvl, keep = "None", other_level = "Drought")) %>% 
-  # filter(state_abb %in% c("DC","NV","AL")) %>%
+         drought_lvl = fct_other(drought_lvl, keep = "None", other_level = "Drought")) %>% # Put all drought lvls together
   group_by(state_abb,map_date) %>% 
   slice_max(order_by = area_pct,n = 1, with_ties = F) %>% 
   ungroup() %>% 
@@ -34,41 +23,23 @@ monthly_drought <- drought %>%
   filter(between(year, 2002,2020))
 
 
-source("2021_w30/theme_tk.R")
-theme_set( theme_tk() + 
-            theme(rect = element_blank(),
-                  panel.border = element_blank(),
-                  strip.background = element_blank(),
-                  panel.grid.major = element_blank(),
-                  axis.title=element_blank(),
-                  axis.text = element_blank(),
-                  legend.direction = "horizontal", 
-                  legend.box = "horizontal",
-                  legend.spacing = unit(1, 'cm'),
-                  plot.margin = unit(c(5,5,5,5), "mm"),
-                  plot.title = element_text(size = 60,
-                                            family="Lato",
-                                            color = "black"),
-                  plot.subtitle = element_text(size = 40,
-                                               family="Lato",
-                                               color = "black"),
-                  strip.text = element_text(face = "bold", size = 20,
-                                            color = "black"),
-                  plot.caption = element_text(size = 30,
-                                              color = "black"),
-                  legend.text = element_text(size = 15,
-                                             color = "black"),
-                  legend.title = element_text(size = 30, color = "black")))
+# Graphic -----------------------------------------------------------------
+# Parameters for grid_panel
+my_gpar <- gpar(
+  col = "black",
+  fontfamily = "Lato Semibold",
+  fontface = "bold.italic",
+  fontsize = 9.5
+)
 
-
-monthly_drought %>% 
-  left_join(states, by = c("state_abb" = "Abbreviation")) %>% 
-  ggplot() +
-  geom_tile(aes(x = month, y = year, fill = None, alpha = None / Total), 
-            col = "grey95", size = .01) + 
+# PLot
+(plot <- monthly_drought %>% 
+  left_join(states, by = c("state_abb" = "Abbreviation")) %>%  
+  ggplot(aes(x = month, y = year)) +
+  geom_tile(aes(fill = None, alpha = None / Total), 
+            color = "black", size = .01) + 
   scale_fill_gradient2(name = "Less Drought-Weeks", 
-                       #trans="sqrt",
-                       low="#1a9641", high=col_cold, mid="#abd9e9",
+                       low="#1a9641", high= "#0067A3", mid="#abd9e9",
                        midpoint = max(monthly_drought$None)/2,
                        breaks = c(min(monthly_drought$None),
                                   max(monthly_drought$None)),
@@ -76,36 +47,75 @@ monthly_drought %>%
                                   max(monthly_drought$None))
   ) + 
   new_scale_fill() +
-  geom_tile(aes(x = month, y = year, fill = Drought, alpha = Drought / Total),
-            col = "grey95", size = .05) +
+  geom_tile(aes(fill = Drought, alpha = Drought / Total),
+            color = "black", size = .05) +
+  labs(
+    title = str_to_upper("Droughts in USA \n 2002 -2020"),
+    subtitle = "Number of weeks per month each state is affected",
+    caption = "Data from ***U.S. Drought Monitor***.<br>
+      Tidytuesday Week-30 2021 &bull; <span style='font-family: \"Font Awesome 5 Brands\"'>&#xf099;</span>**@issa_madjid** inspired by Taras Kaduk." 
+  ) + 
   scale_fill_gradient2(name = "More Drought-Weeks",
-                       #trans="sqrt",
-                       low="#1a9641", high=col_hot, mid = "#fee08b",
+                       low="#1a9641", high="#B8001F", mid = "#fee08b",
                        midpoint = max(monthly_drought$Drought)/2,
                        breaks = c(min(monthly_drought$Drought),
                                   max(monthly_drought$Drought)),
                        limits = c(min(monthly_drought$Drought),
                                   max(monthly_drought$Drought))
   ) +
-  # expand_limits(y = min(monthly_drought$year) - length(unique(monthly_drought$year))) +
-  scale_alpha(guide = 'none')  + 
+  scale_x_continuous(
+    breaks = seq(1,12, by = 3), # Year months by 3 months step
+    labels = function(x) {str_to_upper(month.abb[x])} # Print the abbrecviations
+  ) +
+  scale_y_continuous(
+    labels = function(x) {str_c("'",str_sub(x, 3, 4))}
+  ) +
+  scale_alpha(guide = 'none') + 
   facet_wrap(vars(State)) + 
   coord_cartesian(expand = F, clip = "off") + 
-  # coord_polar() + 
   grid_panel(
     grob = function(data, coords) {
-      if (data$PANEL[1] == 1) {
+      if (data$PANEL[1] == 2) {
         gList(
           textGrob(
-            label = "Deer in Dwelling ?\n Are you serious?",
+            label = "Alaska is the most untouched \n state in the country over\n the last two decades.",
             x = unit( .5, "npc"),
-            y = unit(0.28, "npc"),
+            y = unit(0.75, "npc"),
             just = c("center", "top"),
-            gp = gpar(
-              col = "black",
-              fontfamily = "Lato Semibold",
-              fontsize = 7.5
-            )
+            gp = my_gpar
+          )
+        )
+      }
+      else if (data$PANEL[1] == 5) {
+        gList(
+          textGrob(
+            label = "Things have gotten worse \nsince 2010 in Salifornia.",
+            x = unit( .5, "npc"),
+            y = unit(0.65, "npc"),
+            just = c("center", "top"),
+            gp =my_gpar
+          )
+        )
+      }
+      else if (data$PANEL[1] == 12) {
+        gList(
+          textGrob(
+            label = "Drought in Hawaii ?",
+            x = unit( .5, "npc"),
+            y = unit(0.65, "npc"),
+            just = c("center", "top"),
+            gp =my_gpar
+          )
+        )
+      }
+      else if (data$PANEL[1] == 52) {
+        gList(
+          textGrob(
+            label = "Things get better in Wyoming \nsince 2010.",
+            x = unit( .5, "npc"),
+            y = unit(0.65, "npc"),
+            just = c("center", "top"),
+            gp =my_gpar
           )
         )
       }
@@ -113,5 +123,45 @@ monthly_drought %>%
         nullGrob()
       }
     },
-  )
-  
+  ) +
+  theme_minimal(base_family = "Lato") + 
+  theme(rect = element_blank(),
+        panel.border = element_rect(color = "black", size = 0.4, fill =NA),
+        strip.background = element_blank(),
+        panel.grid = element_blank(),
+        axis.title=element_blank(),
+        axis.text = element_text(color = "black"),
+        legend.direction = "horizontal", 
+        legend.box = "horizontal",
+        legend.spacing = unit(1, 'cm'),
+        plot.margin = unit(c(5,5,5,5), "mm"),
+        plot.title = element_text(size = 25,
+                                  family="Lato Black",
+                                   hjust = .5),
+        plot.subtitle = element_text(size = 20,
+                                     color = "grey25", family = "Lato Semibold",
+                                     face = "italic",
+                                     hjust = .5, margin = margin(t = 15, b = 15)),
+        strip.text = element_text(face = "bold", size = 12,
+                                  color = "black"),
+        plot.caption = element_markdown(color = "grey15", size = rel(1), margin = margin(t = 10,b = 10)),
+        legend.text = element_text(size = 7.5,
+                                   color = "black"),
+        legend.title = element_text(size = 15, color = "black"),
+        line = element_line(colour = "Dark"),
+        text = element_text(colour = "#2A2E45"),
+        axis.ticks = element_blank(),
+        legend.position="bottom"
+        )
+)
+
+
+# Saving ------------------------------------------------------------------
+png(here::here("2021_w30/tidytuesday_2021_w30.png"), width = 15, height = 14, units = "in",res = 320, type = "cairo")
+plot
+dev.off()
+
+# ALT TEXT 
+# This graphic is  Abdoul ISSA BIDA submission for the  Tidytuesday Challenge for 2021 Week 30.
+# The plot is  a facet of  monthly number of drought weeks in each states between 2002 & 2020.
+# Data comes from us Drought Monitor.
