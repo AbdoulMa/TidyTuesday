d886a503c530844674d66081344db6dc26c3776c
AbdoulMa <issamadjid1995@gmail.com>
2021-07-29 23:21:46 +0200

    Function to generate week Skeleton.

---
+++ generate_week_structure.R
@@ -0,0 +1,45 @@
+# Inspired by (Georgios Karamanis) https://github.com/gkaramanis/tidytuesday/blob/master/newweek.R
+library(purrr)
+library(here)
+library(fs)
+library(lubridate)
+
+generate_tidytuesday_structure <- function(week, year) {
+  directory <- paste(year,paste0("w",week),sep = "_")
+  script_file <- paste("tidytuesday", paste0(directory,".R"), sep = "_")
+  image_file <- paste("tidytuesday", paste0(directory,".png"), sep = "_")
+  if (!dir_exists(here(directory))) {
+    dir_create(here(directory))
+    
+    # Create Script file & write  skeleton 
+    file_create(here(directory, script_file))
+    script_text <- paste0(
+      '# Load libraries ----------------------------------------------------------\n',
+      'library(tidyverse)',
+      '\n\n',
+      '# Data Reading and Wrangling ----------------------------------------------\n',
+      '\n\n',
+      '# Graphic -----------------------------------------------------------------\n',
+      '\n\n',
+      '# Saving ------------------------------------------------------------------'
+    )
+    write(as.character(script_text), file(here(directory, script_file)))
+    
+    # Create README file & write  skeleton 
+    file_create(here(directory, "README.md"))
+    write(as.character(paste0("![](",image_file,")")), file(here(directory, "README.md")))
+    
+    # Open Script 
+    file.edit(here(directory, script_file))
+  }
+}
+
+# 
+# walk2(
+#   19:22,
+#   2021,
+#   ~generate_tidytuesday_structure(.x, .y)
+# )
+
+generate_tidytuesday_structure(32,2021)
+
