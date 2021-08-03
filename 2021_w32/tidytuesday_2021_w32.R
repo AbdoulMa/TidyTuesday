73292a570e77fe692b63f92b188359302343929f
AbdoulMa <issamadjid1995@gmail.com>
2021-08-03 19:33:58 +0200

    Some fixings and commenting.

--- 2021_w32/tidytuesday_2021_w32.R
+++ 2021_w32/tidytuesday_2021_w32.R
@@ -4,56 +4,43 @@ library(ggtext)
 library(gganimate)
 # Data Reading and Wrangling ----------------------------------------------
 athletes <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-08-03/athletes.csv')
- athletes <- athletes %>% 
+athletes <- athletes %>% 
   mutate(abb = case_when(abb == "URS" ~ "RUS",
                          abb == "FRG" ~ "GER",
                          TRUE ~ abb))
- 
-# Get Paralympic Games Editions
+
+# Get Paralympic Games Editions from last week Dataset
 olympics <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-27/olympics.csv')
 games_editions <- olympics %>% 
- filter(season == "Summer") %>% 
- distinct(year, city) %>% 
+  filter(season == "Summer") %>% 
+  distinct(year, city) %>% 
   mutate(edition = glue::glue('{city} <br> <span style = "color: grey35;">{year}</span>')) %>% 
- filter(between(year, 1980,2016)) %>% 
- arrange(year)
- 
+  filter(between(year, 1980,2016)) %>% 
+  arrange(year)
 
- top_federations <- athletes %>% 
-   filter(medal == "Gold") %>% 
-   count(abb, sort = T) %>% 
-   head(20)
+# Retrieve best federations 
+top_federations <- athletes %>% 
+  filter(medal == "Gold") %>% 
+  count(abb, sort = T) %>% 
+  head(20)
 
- top_federations_list <- top_federations %>% 
-   pull(abb)
- 
- top_federations_list
-  medals_records <- athletes %>% 
+# Pull federations
+top_federations_list <- top_federations %>% 
+  pull(abb)
+
+# Establish countries ranking per edition
+medals_records <- athletes %>% 
   filter(medal == "Gold", abb %in% top_federations_list) %>% 
-  distinct(abb, year,event , .keep_all = T) %>% 
+  distinct(abb, year,event , .keep_all = T) %>% # (One gold medal for collective sport)
   count(abb, year,  sort = T)  %>% 
-    complete(abb, year, fill = list(n = 0)) %>%
-    group_by(year) %>%
+  complete(abb, year, fill = list(n = 0)) %>%
+  group_by(year) %>%
   mutate(rk = rank(-n, ties.method = "first")) %>%
   ungroup(year) %>%
   arrange(year)
-  
-medals_records
-
-
-# top_federations <- medals_records %>% 
-#   count(abb, wt = n, sort = T) %>% 
-#   head(6) %>% 
-#   select(-n) %>% 
-#   mutate(
-#     primary_clr = c("#3c3b6e", "#DE2910","#001f7e", "#000000","#ff0000","#0055A4"),
-#     secondary_clr = c("#b22234","#FFDE00","#FFFFFF","#ffce00", "#FFFFFF","#EF4135")
-#   )
-
-
 
 # Graphic -----------------------------------------------------------------
-top_three <- c("USA","CHN","AUS")
+highlight_federations <- c("USA","CHN","AUS")
 plot <- medals_records %>% 
   ggplot() + 
   geom_line(data = medals_records %>%
@@ -62,18 +49,18 @@ plot <- medals_records %>%
                 tibble(year = seq(min(.$year), max(.$year),length.out=100),
                        rk = pracma::pchip(.$year, .$rk, year))
               }),
-    aes(year, rk, group = abb, alpha = ifelse(abb %in% top_three, 1, .25),
-                color = ifelse(abb %in% top_three, abb, NA),
-                size = ifelse(abb %in% top_three, 2.5, 1.5))) +
-  geom_point(aes(year, rk, color = ifelse(abb %in% top_three, abb, NA), 
-                 stroke = ifelse(abb %in% top_three, 1.75, .75),
-             size =  ifelse(abb %in% top_three, 11.5, 8.5),
-             group = seq_along(year)
-             ),
-             shape = 21, fill = "white") + 
+            aes(year, rk, group = abb, alpha = ifelse(abb %in% highlight_federations, 1, .25),
+                color = ifelse(abb %in% highlight_federations, abb, NA),
+                size = ifelse(abb %in% highlight_federations, 2.5, 1.5))) +
+  geom_point(aes(year, rk, color = ifelse(abb %in% highlight_federations, abb, NA), 
+                 stroke = ifelse(abb %in% highlight_federations, 1.75, .75),
+                 size =  ifelse(abb %in% highlight_federations, 11.5, 8.5),
+                 group = seq_along(year)
+  ),
+  shape = 21, fill = "white") + 
   geom_text(aes(year, rk, label = abb, group = seq_along(year),
-                size =  ifelse(abb %in% top_three, 3.75, 2.75),
-                color = ifelse(abb %in% top_three, abb, NA)), 
+                size =  ifelse(abb %in% highlight_federations, 3.75, 2.75),
+                color = ifelse(abb %in% highlight_federations, abb, NA)), 
             family = "Lato Black",
             fontface = "bold") + 
   geom_richtext(data = games_editions, aes(x = year, y = 22, group = seq_along(year), label = edition),
@@ -103,7 +90,7 @@ plot <- medals_records %>%
     axis.text.x = element_blank(),
     axis.text.y =  element_text(size = 15, color = "black", family = "Inconsolata", face = "bold"),
     axis.title = element_blank(),
-    plot.title = element_markdown(color = "black", family = "Lato Black", size = rel(2), margin = margin(t = 5,b = 10), hjust = .5),
+    plot.title = element_markdown(color = "black", family = "Lato Black", size = rel(2), margin = margin(t = 5,b = 5), hjust = .5),
     plot.caption = element_markdown(color = "black", size = rel(1.2), margin = margin(t = 20,b = 10)),
     plot.margin = margin(t = 10,l = 15,r = 15)
   ) 
@@ -122,5 +109,5 @@ pdftools::pdf_convert(pdf = glue::glue("{path}.pdf"),
 animation <- plot + 
   transition_reveal(year)
 
-animate(animation, 10*5, fps = 5 , duration = 30, width = 1200, height = 800, end_pause = 10, 
+animate(animation, 10*5, fps = 5 , duration = 30, width = 1200, height = 950, end_pause = 10, 
         renderer = gifski_renderer(glue::glue("{path}.gif")))
