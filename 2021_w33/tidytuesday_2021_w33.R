d82237c278e3f8514b5aaa903592a4a11107411b
AbdoulMa <issamadjid1995@gmail.com>
2021-08-10 17:00:15 +0200

    Plot finished.

--- 2021_w33/tidytuesday_2021_w33.R
+++ 2021_w33/tidytuesday_2021_w33.R
@@ -1,10 +1,58 @@
 # Load libraries ----------------------------------------------------------
 library(tidyverse)
+library(waffle)
+library(ggtext)
 
 # Data Reading and Wrangling ----------------------------------------------
+investment <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-08-10/investment.csv')
 
+main_investments <- investment %>%
+  filter(!str_detect(meta_cat, "infrastructure")) %>% 
+  mutate(meta_cat = str_wrap(str_to_upper(meta_cat), width = 20)) %>% 
+  count(meta_cat, wt = gross_inv, sort = T)
+
+main_categories <- main_investments %>% 
+  pull(meta_cat)
+
+main_categories
+categories_investments <- main_investments  %>% 
+  mutate(prop = MESS::round_percent(prop.table(n)),
+         remaining_prop = 100-prop) %>% 
+  pivot_longer(cols = c("prop", "remaining_prop"), names_to = "proportions", values_to = "prop")
+categories_investments 
 
 # Graphic -----------------------------------------------------------------
-
+categories_investments %>% 
+  mutate(meta_cat = fct_relevel(meta_cat, main_categories))%>% 
+  ggplot(aes(fill = proportions, values = prop)) +
+  geom_waffle(color = "white", size = .15, n_rows = 10, flip = T) + 
+  scale_fill_manual(
+    values = c(
+      "prop" = "#009E73",
+      "remaining_prop" = "grey90"),
+    guide = "none"
+  ) + 
+  labs(
+    title = "For every $100 invested, \nhow much is in key sectors?",
+    caption = "Data from ***Bureau of Economic Analysis***.<br>
+       Tidytuesday Week-33 2021 &bull;<span style='font-family: \"Font Awesome 5 Brands\"'>&#xf099;</span>**@issa_madjid**."
+  ) + 
+  coord_equal(expand = F) + 
+  facet_wrap(vars(meta_cat)) + 
+  theme_minimal(base_family = "Lato Semibold") + 
+  theme(
+    plot.title = element_text(family = "Lato Black",size = rel(2.5), hjust = .5, margin = margin(b = 10)),
+    panel.border = element_rect(color = "black", size = 1.1, fill = NA),
+    axis.text = element_blank(),
+    strip.text = element_text(size= rel(1.1)),
+    plot.caption = element_markdown(color = "black", size = rel(1.2), margin = margin(t = 20,b = 10)),
+    plot.margin = margin(5, unit = "mm")
+  )
 
 # Saving ------------------------------------------------------------------
+path <- here::here("2021_w33/tidytuesday_2021_w33")
+ggsave(glue::glue("{path}.pdf"), width = 9, height = 12, device = cairo_pdf)
+
+pdftools::pdf_convert(pdf = glue::glue("{path}.pdf"), 
+                      filenames = glue::glue("{path}.png"),
+                      format = "png", dpi = 96)
