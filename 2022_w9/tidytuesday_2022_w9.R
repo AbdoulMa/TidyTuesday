0695713426e794331ad0b362c84e3c0ebb9bbe2b
AbdoulMa <issamadjid1995@gmail.com>
2022-03-01 21:59:18 +0100

    Improvment to W9 Plot.

--- 2022_w9/tidytuesday_2022_w9.R
+++ 2022_w9/tidytuesday_2022_w9.R
@@ -22,7 +22,9 @@ states <- tigris::states() %>%
 
 
 stations <- stations %>% 
-  filter(LONGITUDE != 0,!STATE %in% ommitted_states) %>% 
+  filter(LONGITUDE != 0,!STATE %in% ommitted_states, 
+         !CITY %in% c("Guaynabo","San Juan","Rio Grande","Carolina"),
+         STATION_NAME != "Niumamua Office Building") %>% 
   mutate(
     LONGITUDE = case_when(ZIP == 98345 ~ -122.6255, 
                           TRUE ~ LONGITUDE),
@@ -32,8 +34,8 @@ stations <- stations %>%
 
 stations_pos <- stations %>% 
   mutate(
-    circle_long = plyr::round_any(LONGITUDE, .25, ceiling),
-    circle_lat = plyr::round_any(LATITUDE, .25, ceiling), 
+    circle_long = plyr::round_any(LONGITUDE, .35, ceiling),
+    circle_lat = plyr::round_any(LATITUDE, .35, ceiling), 
     .before = 1L
   ) %>% 
   group_by(circle_long, circle_lat, EV_NETWORK) %>% 
@@ -52,27 +54,29 @@ glimpse(stations_pos)
 stations_geoms <- stations_pos %>% 
   st_as_sf(coords = c("circle_long", "circle_lat")) %>% 
   st_set_crs(4326) %>% 
-  st_transform("ESRI:102003") %>%
-  shift_geometry()
+  st_transform("ESRI:102003") 
 
 
+
+# Graphic -----------------------------------------------------------------
 (plot <- ggplot() + 
-    geom_sf(data = filter(states, !STUSPS %in% c("HI","PR")), fill = NA, size = .1) +
-    geom_sf(data = stations_geoms, aes(color = EV_NETWORK), size = .9) + 
-    theme_minimal()+ 
-    theme(
-      panel.grid = element_blank(),
-      legend.position = "none",
-      legend.title = element_blank(), 
-      axis.text = element_blank()
-    ))
+   geom_sf(data = filter(states, !STUSPS %in% c("HI","PR")), fill = NA, size = .1) +
+   geom_sf(data = stations_geoms, aes(color = EV_NETWORK), size = 1.35) + 
+   theme_minimal()+ 
+   theme(
+     panel.grid = element_blank(),
+     legend.position = "none",
+     legend.title = element_blank(), 
+     axis.text = element_blank()
+   ))
 
 networks <- levels(stations_geoms$EV_NETWORK)
+
 # 1 deg -> 54.6 miles
-# .25 de -> 54.6/4 = 13.5 miles 
+# .35 de -> 54.6*.35 = 13.5 miles 
 # 10 / 54.6 = .18 deg -> 10 miles 
 (legend_plot <- ggplot() + 
-    geom_text(data = tibble(networks), aes(x = 1:10, y = 1, label = networks)) + 
+    geom_text(data = tibble(networks), aes(x = 1:10, y = 1, label = networks), size = 2.5) + 
     geom_point(aes(x = 1:10, y = 0, color = networks), size = 3.5) + 
     coord_cartesian(clip = "off") + 
     theme_minimal() + 
@@ -86,25 +90,27 @@ networks <- levels(stations_geoms$EV_NETWORK)
 
 
 combine_plot <- legend_plot + plot +
-  plot_layout(ncol = 1,heights = c(.05,.95))
+  plot_layout(ncol = 1,heights = c(.035,.965))
 
 
-caption <- "Data from US DOT by way of Data is Plural. \n\nTidytuesday Week-8 2022 \n Abdoul ISSA BIDA"
+caption <- "Data from US DOT \nby way of Data is Plural. \n \nTidytuesday Week-8 2022 \nAbdoul ISSA BIDA\ninspired by Nathan YAU."
 ggdraw(combine_plot) + 
-  draw_label(x = .15, y = .2, 
+  draw_label(x = .1, y = .2, 
              label = caption,
+             size = 10,
              hjust = 0
-             ) + 
+  ) + 
   labs(title = "Nearest Stations", 
-       subtitle = "The most common alternative transportation fuel stations\n within a 10 miles radius") + 
+       subtitle = "The most common alternative transportation fuel stations\n within a 20 miles radius") + 
   theme_minimal() + 
-  theme()
-# Graphic -----------------------------------------------------------------
-
+  theme(
+    plot.background = element_rect(fill = "white", color = NA),
+    plot.margin = margin(t = 1, r= .5, b=.5, l=.5, unit = "cm")
+  )
 
 # Saving ------------------------------------------------------------------
 path <- here::here("2022_w9", "tidytuesday_2022_w9")
-ggsave(filename = glue::glue("{path}.png"), width = 11, height = 8.5, device = ragg::agg_png, dpi = 300)
+ggsave(filename = glue::glue("{path}.png"), width = 10.5, height = 8, device = ragg::agg_png, dpi = 300)
 
 
 
