b57dbd76b0db5d424c2e3320a938c889f2161ad8
AbdoulMa <issamadjid1995@gmail.com>
2022-05-17 20:14:52 +0200

    Graphics.

--- 2022_w20/tidytuesday_2022_w20.R
+++ 2022_w20/tidytuesday_2022_w20.R
@@ -9,11 +9,11 @@ eurovision_2022 <- eurovision_votes %>%
   filter(year == 2022, semi_final == 'f') %>% 
   group_by(from_country) %>% 
   mutate(rk_grp = case_when(points == 12 ~ "1st", 
-                          points == 10 ~ "2nd",
-                          points == 8 ~ "3rd", 
-                          between(points, 4, 7) ~ "4th-7th", 
-                          between(points, 1, 3) ~ "8th-10th", 
-                          TRUE ~ "Not classed"
+                            points == 10 ~ "2nd",
+                            points == 8 ~ "3rd", 
+                            between(points, 4, 7) ~ "4th-7th", 
+                            between(points, 1, 3) ~ "8th-10th", 
+                            TRUE ~ "Not classed"
   ), 
   rk_grp = fct_relevel(rk_grp, levels = c("1st","2nd","3rd", "4th-7th", "8th-10th", "Not classed")))
 
@@ -22,9 +22,14 @@ eurovision_2022_standings <- eurovision_2022 %>%
   summarize(total_points = sum(points)) %>% 
   ungroup() %>% 
   mutate(event_rk = rank(desc(total_points), ties.method = "first"), 
-         fancy_rk = glue::glue("#{event_rk}, {to_country}")) 
-
-eurovision_2022_pts_prop <- eurovision_2022 %>% 
+         fancy_rk = glue::glue("#{event_rk}, {to_country} - {total_points} pts"),
+         fancy_rk= fct_reorder(fancy_rk, event_rk)) %>% 
+  relocate(
+    c("event_rk", "fancy_rk"),
+    .before = 1
+  )  
+  
+  eurovision_2022_pts_prop <- eurovision_2022 %>% 
   group_by(to_country, jury_or_televoting) %>% 
   summarise(nb_points = sum(points)) %>% 
   ungroup() 
@@ -37,7 +42,7 @@ final_eurovision_2022_pts_prop <- eurovision_2022_pts_prop %>%
   replace_na(list(prop_start = 0))  %>% 
   left_join(eurovision_2022_standings)
 
-eurovision_2022 %>% 
+final_eurovision_votes <- eurovision_2022 %>% 
   group_by(to_country, rk_grp) %>% 
   count() %>% 
   ungroup() %>% 
@@ -52,15 +57,34 @@ eurovision_2022 %>%
   select(-c(n, indice_start)) %>% 
   mutate(
     xind = indice %% 13,
-  )
-
+    yind = indice %/% 13
+  ) %>% left_join(eurovision_2022_standings)
 
 # Graphic -----------------------------------------------------------------
-final_eurovision_2022_pts_prop %>% 
-  ggplot() + 
-  geom_rect(aes(xmin = prop_start, xmax = prop_start + points_prop, ymin = 0, ymax = 1, fill = jury_or_televoting)) + 
-  facet_wrap(vars(fancy_rk))
 
+ggplot() + 
+  ggforce::geom_circle(data = final_eurovision_votes,  aes(x0 = xind, y0=yind, r = .5, fill = rk_grp)) +
+  geom_rect(data = final_eurovision_2022_pts_prop, aes(xmin = 13*prop_start -.5, xmax = 13*(prop_start + points_prop) -.5, ymin = -3, ymax = -1, fill = jury_or_televoting)) + 
+  facet_wrap(vars(fancy_rk)) + 
+  scale_fill_manual(
+    values = c("1st" = "#d11513",
+               "2nd" = "#f34b26",
+               "3rd" = "#f1b813",
+               "4th-7th" = "#046e46",
+               "8th-10th" = "#037bc7",
+               "Not classed" = "#35008d",
+               "T" = "#111111",
+               "J" = "#AAAAAA")
+  ) + 
+  coord_equal() + 
+  theme_minimal() + 
+  theme(
+  axis.text = element_blank(), 
+  axis.title = element_blank(), 
+  panel.grid = element_blank(), 
+  legend.position = "none", 
+  strip.text = element_text(family = "Gotham Condensed", size = 16, face = "bold", hjust = 0)
+  )
 
 # Saving ------------------------------------------------------------------
 path <- here::here("2022_w20", "tidytuesday_2022_w20")
