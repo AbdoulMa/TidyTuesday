e73ec5ee919a8a2fc6b9b2f48ac40ada6356cfcf
AbdoulMa <issamadjid1995@gmail.com>
2022-08-23 22:29:50 +0200

    Finish main idea.

--- 2022_w34/tidytuesday_2022_w34.R
+++ 2022_w34/tidytuesday_2022_w34.R
@@ -1,7 +1,7 @@
 
 # Load Libraries ----------------------------------------------------------
 library(tidyverse)
-library(ggbeeswarm)
+library(patchwork)
 
 
 # Data Wrangling ----------------------------------------------------------
@@ -25,11 +25,17 @@ daily_releases <- chips |>
 dates_range <- range(daily_releases$release_date, na.rm = T)
 start_date <- format(dates_range[1], "%m.%d.%Y" )
 end_date <- format(dates_range[2], "%m.%d.%Y" )
-  
-daily_releases |> 
+
+(daily_releases_plot <- daily_releases |> 
   ggplot(aes(release_date, Total)) + 
   geom_point(aes( shape = Type, fill = Vendor), color = "white", size = 2.5) + 
   geom_smooth(aes(color = Vendor), se = F) + 
+  labs(
+    subtitle = "The number of transitions in a dense integrated circuit (IC)\n doubles about every two years, improving
+    preformances both on CPUs and GPUs.",
+    x = "Release date",
+    y = "Nb releases\n in millions"
+  ) + 
   facet_wrap(vars(Vendor), nrow = 1) + 
   scale_shape_manual(
     values = c(
@@ -38,30 +44,34 @@ daily_releases |>
     )
   ) +
   scale_y_continuous(
+
     trans = scales::log10_trans(),
     labels = scales::label_comma()
   ) + 
   coord_cartesian(clip = "off") + 
   theme_minimal() +
   theme(
+    legend.position = "none",
     panel.grid.minor  = element_blank(),
     panel.grid.major = element_line(size = .2, color = "#111111", linetype = "dashed"),
     panel.background = element_rect(fill = "#FFFFFF", color = NA), 
     panel.spacing.x = unit(.5, "cm"),
-    strip.text = element_text(size = rel(2.5), margin = margin(b = .5, unit ="cm")),
-    axis.text = element_text(color = "#111111", size = rel(1.125)),
-    axis.title.y = element_text(angle = 0, vjust = .5),
+    plot.subtitle = element_text(margin = margin(t= .75, b = .75, unit = "cm")),
+    strip.text = element_text(size = rel(3), family = "UEFA Supercup", face = "bold", margin = margin(b = .5, unit ="cm")),
+    axis.text = element_text(size = rel(1.125), family = "Inconsolata"),
+    axis.title = element_text(size = rel(1.5), family = "UEFA Supercup"),
+    axis.title.y = element_text(angle = 0, vjust = 1.1, margin = margin(r = -50, unit = "pt")),
     axis.ticks = element_line(size = unit(.25, "pt")),
     axis.ticks.length.x  =  unit(3, "pt"),
     axis.ticks.length.y  =  unit(5, "pt"),
     axis.line.x = element_line(size = unit(.25, "pt"))
   )
-
+)
 
 # Second facet plot 
-stylize_vendor_text <- \(Vendor) {glue::glue("<span style='font-weight:bold;font-size: 45px;'>{Vendor}</span>")}
+stylize_vendor_text <- \(Vendor) {glue::glue("<span style='font-family: \"UEFA Supercup\"; font-weight:bold; font-size: 45px;'>{Vendor}</span>")}
 
-stylize_summary_text <- \(transistors_total, Category_prop_GPU, Category_prop_CPU) {glue::glue("<span style='font-weight:bold;font-size: 35px;'>{scales::label_comma(suffix = 'M')(transistors_total)}</span> <br> {Category_prop_GPU} {Category_prop_CPU}</span>")}
+stylize_summary_text <- \(transistors_total, Category_prop_GPU, Category_prop_CPU) {glue::glue("<span style='font-family: \"UEFA Supercup\";'><span style='font-weight:bold;font-size: 35px;'>{scales::label_comma(suffix = 'M')(transistors_total)}</span> <br> {Category_prop_GPU} {Category_prop_CPU}</span>")}
 
 daily_releases_summary <-  daily_releases |> 
   count(Vendor, Type, wt = Total, name = "Category_total") |> 
@@ -83,32 +93,46 @@ daily_releases_summary <-  daily_releases |>
   )
 
 overall_realeases <- scales::label_comma(suffix = " Millions")(sum(daily_releases_summary$transistors_total))
-daily_releases_summary |> 
+(summary_plot <- daily_releases_summary |> 
   ggplot() + 
-geom_rect(
-  aes(fill = Vendor), 
-  xmin = -1, ymin = -1, xmax = 1, ymax = 1, color = "white"
-) + 
+  geom_rect(
+    aes(fill = Vendor), 
+    xmin = -1, ymin = -1, xmax = 1, ymax = 1, color = "white"
+  ) + 
   ggtext::geom_richtext(aes(label = fancy_summary),
-                hjust = 0.5,
-                size = 5,
-                color = "white",
-                x = 0, y = 0.05,
-                lineheight = 1.5,
-                fontface = "bold",
-                fill = NA, label.colour = NA, show.legend = F) + 
+                        hjust = 0.5,
+                        size = 5,
+                        color = "white",
+                        x = 0, y = 0.05,
+                        lineheight = 1.5,
+                        fontface = "bold",
+                        fill = NA, label.colour = NA, show.legend = F) + 
   labs(
-    title = glue::glue("{overall_realeases} of chips have been released between {start_date} and {end_date} \n by the major manufacturers")
+    subtitle = glue::glue("{overall_realeases} of chips have been released between {start_date} and {end_date} \n by the major manufacturers")
   ) + 
   coord_fixed(xlim =c(-1,1), ylim = c(-1,1)) + 
   # scale_fill_manual(values = type_fillings, 
-                    # guide = "none") + 
+  # guide = "none") + 
   theme_minimal() + 
-  theme(panel.spacing.x = unit(-5,"points"),
-        plot.title = element_text(size = 25, color = "black", hjust = .5),
-        strip.text = element_blank()) +
+  theme(
+    legend.position = "none",
+    panel.spacing.x = unit(-5,"points"),
+    plot.subtitle = element_text( margin = margin(t= .5, unit = "cm")),
+    strip.text = element_blank()) +
   facet_wrap(vars(Vendor), nrow = 1)
+)
 
+caption <- "Data from **CHIP Dataset** <br> Tidytuesday Week-34 2022 &bull;  Abdoul ISSA BIDA  <span style='font-family: \"Font Awesome 5 Brands\"'>&#xf099;</span>**@issa_madjid**."
+daily_releases_plot / summary_plot + 
+  plot_annotation(
+    title = "Chips Manufacturing",
+    caption = caption
+  ) &
+  theme(
+    plot.title = element_text(size = rel(3.5), family = "UEFA Supercup", face = "bold", hjust = .5),
+    plot.subtitle = element_text(size = 25, color = "#111111", family =  "NY Bold Italic", hjust = .5),
+    plot.caption = ggtext::element_markdown(family = "UEFA Supercup", size = rel(1.5), hjust = .5)
+  )
 
 # Saving ------------------------------------------------------------------
 path <- here::here("2022_w34", "tidytuesday_2022_w34")
