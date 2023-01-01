f4ac5d91e28ffa14648245de29be8af78369adba
AbdoulMa <issamadjid1995@gmail.com>
2023-01-01 21:10:43 +0100

    Stylize file.

--- generate_week_structure.R
+++ generate_week_structure.R
@@ -5,37 +5,36 @@ library(fs)
 library(lubridate)
 
 generate_tidytuesday_structure <- function(week, year) {
-  directory <- paste(year,paste0("w",week),sep = "_")
-  script_file <- paste("tidytuesday", paste0(directory,".R"), sep = "_")
-  image_file <- paste("tidytuesday", paste0(directory,".png"), sep = "_")
+  directory <- paste(year, paste0("w", week), sep = "_")
+  script_file <- paste("tidytuesday", paste0(directory, ".R"), sep = "_")
+  image_file <- paste("tidytuesday", paste0(directory, ".png"), sep = "_")
   # Create year dirextory if not exists
   if (!dir_exists(as.character(year))) {
     dir_create(as.character(year))
   }
-  
+
   # Files paths
-  week_directory_path <-  here(year, directory)
+  week_directory_path <- here(year, directory)
   week_script_path <- here(week_directory_path, script_file)
   week_readme_path <- here(week_directory_path, "README.md")
-  
+
   if (!dir_exists(week_directory_path)) {
     dir_create(week_directory_path)
-    
-    # Create week script file  
+
+    # Create week script file
     file_create(week_script_path)
-  
-    # Generate Week script file from skeleton  
+
+    # Generate Week script file from skeleton
     week_skeleton <- whisker::whisker.render(readr::read_lines("week_skeleton.R"))
     readr::write_lines(week_skeleton, week_script_path)
-    
-    # Create README file & write skeleton 
+
+    # Create README file & write skeleton
     file_create(week_readme_path)
-    readr::write_lines(paste0("![](",image_file,")"), week_readme_path)
-    
-    # Open Script 
+    readr::write_lines(paste0("![](", image_file, ")"), week_readme_path)
+
+    # Open Script
     file.edit(week_script_path)
   }
 }
 
-generate_tidytuesday_structure(1,2023)
-
+generate_tidytuesday_structure(1, 2023)
