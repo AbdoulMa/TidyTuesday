de914d64325e8d3d93304dc83e2ee2a3ea911bb6
AbdoulMa <issamadjid1995@gmail.com>
2023-02-07 17:54:58 +0100

    Week 6 completed.

--- 2023/2023_w6/tidytuesday_2023_w6.R
+++ 2023/2023_w6/tidytuesday_2023_w6.R
@@ -2,39 +2,90 @@
 # Load Libraries ----------------------------------------------------------
 library(tidyverse)
 library(ggtext)
-
+library(ggimage)
 # Data Wrangling ----------------------------------------------------------
-big_tech_stock_prices <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-07/big_tech_stock_prices.csv')
+big_tech_companies <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-07/big_tech_companies.csv')
+# Assign companies colors 
+big_tech_companies$clr <- c("#A2AAAD", "#FF0000","#FF9900", "#1798c1", "#00BCEB","#34A853", "#0530AD", "#0068B5","#0668E1","#F14F21", "#E50914", "#76B900", "#C74634","#E31937")
+# Companies logos (downloades in folder Logos)
+big_tech_companies <- big_tech_companies |> 
+  mutate(lower_stock = tolower(stock_symbol),
+         company_logo_path = paste0(here::here("2023/2023_w6/Logos/"),lower_stock, '.png'))
 
-images_positions <- big_tech_stock_prices |> 
-  group_by(stock_symbol) |> 
+big_tech_stock_prices <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-07/big_tech_stock_prices.csv')
+big_tech_stock_prices <- big_tech_stock_prices |> 
+  left_join(big_tech_companies)
+
+companies_logos_df <- big_tech_stock_prices |> 
+  group_by(stock_symbol, company_logo_path) |> 
   summarise(
+    first_date = min(date),
     max_adj_close = max(adj_close)
   ) |> 
-  ungroup() |> 
-  mutate(
-    x_pos = ymd("2010-02-10")
-  )
+  ungroup() 
 
 
- # |> 
-  # mutate(year = lubridate::year(date)) |> 
-  # filter(year == 2022) |> 
-  ggplot() + 
-  geom_line(data = big_tech_stock_prices, aes(date, adj_close, color = stock_symbol, group = stock_symbol)) + 
-  geom_text(data = images_positions, aes(x = x_pos, max_adj_close, label= stock_symbol )) +
-  facet_wrap(vars(stock_symbol),scales = "free_y", nrow = 2)
 
 # Graphic -----------------------------------------------------------------
+(main_plot <- ggplot() + 
+  geom_line(data = big_tech_stock_prices, aes(date, adj_close, color = clr, group = stock_symbol), size = .75) + 
+    geom_ribbon(data = big_tech_stock_prices, aes(date, ymin = 0, ymax = adj_close, fill = clr), alpha = .6) + 
+    geom_richtext(data = companies_logos_df, aes(x = first_date, y = max_adj_close, label = glue::glue("<img src='{company_logo_path}' width='40'/>")), 
+                  hjust = 0,
+                  fill = NA,
+                  label.padding = unit(.125, 'lines'),
+                  label.r = unit(0, 'lines'),
+                  label.size = unit(0, 'lines')
+                  ) +
+  labs(
+    title = "Big Tech companies", 
+    subtitle = "Closing Stock prices evolution between 2010 & 2022"
+  ) + 
+  scale_color_identity() + 
+    scale_fill_identity() + 
+    coord_cartesian(clip = "off", expand = F) + 
+  facet_wrap(vars(stock_symbol), scales = "free", nrow = 5) + 
+  theme_minimal() + 
+  theme(
+    plot.title = element_text(color = "black", face = "bold", family = "Go Black",  size = rel(3.5), margin = margin(b = .5, unit = "cm")),
+    plot.subtitle = element_text(color = "#333333", family = "Go Bold", size = rel(1.75), margin = margin(b = 1, unit = "cm")),
+    strip.text = element_blank(),
+    panel.spacing.x =  unit(1, "cm"),
+    panel.spacing.y =  unit(1, "cm"),
+    panel.grid = element_blank(),
+    axis.title = element_blank(),
+    axis.line.x = element_line(),
+    axis.text.x = element_text(color = "black", family = "Iosevka", size = rel(1.25)),
+    axis.text.y = element_blank(),
+    axis.ticks.length = unit(.125, "cm"), 
+    axis.ticks.x = element_line(size = .25),
+    plot.background = element_rect(fill = "white", color = NA)
+  )
+)
 
+(caption_plot <- ggplot() + 
+  annotate(geom = "richtext", x = 0, y = 0, label = "Tidytuesday Week-06 2023 <br>Data from: Yahoo Finance<br>Abdoul ISSA BIDA <span style='font-family: \"Font Awesome 5 Brands\"'>&#xf099;</span>**@issa_madjid**",
+          family = "Iosevka Semibold", 
+           size = 5.5,
+           fill = NA, 
+           label.size = unit(0, 'pt'),
+           label.r = unit(0,'pt')) + 
+  theme_minimal() + 
+  theme(
+    panel.grid = element_blank(),
+    plot.background = element_rect(fill = "white", color = NA),
+    axis.text =  element_blank(),
+    axis.title =  element_blank()
+  )
+)
 
+main_plot + 
+  patchwork::inset_element(caption_plot,  left = 0.66, bottom = 0, right = 0.999, top = 0.15) & 
+  theme(
+    plot.margin = margin(c(.25, .25,.25,.25), unit = "cm")
+  )
 # Saving ------------------------------------------------------------------
 path <- here::here("2023", "2023_w6", "tidytuesday_2023_w6")
-ggsave(filename = glue::glue("{path}.pdf"), width = 9, height = 9, device = cairo_pdf)
+ggsave(filename = glue::glue("{path}.png"), width = 12.5, height = 9.5, device = ragg::agg_png, dpi = 300)
 
-pdftools::pdf_convert(
-  pdf = glue::glue("{path}.pdf"),
-  filenames = glue::glue("{path}.png"),
-  dpi = 320
-)
 
