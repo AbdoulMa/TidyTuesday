e2d9d105d401f6555ce345aa0370c8e127aae88c
AbdoulMa <issamadjid1995@gmail.com>
2023-04-01 11:39:36 +0200

    Small fixings.

--- 2023/2023_w11/tidytuesday_2023_w11.R
+++ 2023/2023_w11/tidytuesday_2023_w11.R
@@ -42,7 +42,7 @@ holders_df <- drugs |>
   )
 # Graphic -----------------------------------------------------------------
 # Drip shape df
-r <- .325
+r <- .25
 m <- (pi * 60.5) / 180 # tangent lines
 xo <- 115 / 10
 yo <- -0.3
@@ -56,9 +56,9 @@ drip_df <- tibble(
 # Plot
 holders_df |>
   ggplot() +
-  geom_rect(aes(xmin = 0, xmax = n / 10, ymin = 0, ymax = 1), fill = "#add8e6", alpha = .65) +
+  geom_rect(aes(xmin = 0, xmax = n / 10, ymin = -.125, ymax = 1.125), fill = "#add8e6", alpha = .65) +
   geom_text(aes(x = 95 / 10, y = .5, label = holder_company_name), color = "white", family = "Go Condensed Medium", fontface = "bold", size = 8.5, hjust = 1) +
-  annotate(geom = "rect", xmin = 0, xmax = 100 / 10, ymin = 0, ymax = 1, color = "white", fill = NA) +
+  annotate(geom = "rect", xmin = 0, xmax = 100 / 10, ymin = -.125, ymax = 1.125, color = "white", fill = NA) +
   annotate(geom = "rect", xmin = -5 / 10, xmax = 0, ymin = .35, ymax = .65, color = "white", fill = "white") +
   annotate(geom = "rect", xmin = -7 / 10, xmax = -5 / 10, ymin = .15, ymax = .85, color = "white", fill = "white") +
   annotate(geom = "rect", xmin = 100 / 10, xmax = 101 / 10, ymin = .35, ymax = .65, color = "white", fill = "white") +
@@ -66,7 +66,7 @@ holders_df |>
   annotate(geom = "segment", x = 102 / 10, xend = 115 / 10, y = .5, yend = .5, color = "white") +
   geom_polygon(data = drip_df, aes(x, y), fill = "white") +
   ggforce::geom_circle(data = tibble(x = xo, y = yo), aes(x0 = xo, y0 = yo, r = r), color = "white", fill = "#add8e6", alpha = .65, size = .85) +
-  geom_text(aes(x = xo, yo, label = n), family = "Inconsolata Semibold", size = 7.5) +
+  geom_text(aes(x = xo, yo, label = n), family = "Inconsolata Semibold", size = 5.5) +
   facet_wrap(vars(holder_company_name), ncol = 1) +
   labs(
     title = "The big pharmaceutical companies",
@@ -84,7 +84,7 @@ holders_df |>
     axis.title = element_blank(),
     axis.text = element_blank(),
     strip.text = element_blank(),
-    panel.spacing.y = unit(.125, "cm"),
+    panel.spacing.y = unit(.025, "cm"),
     panel.grid = element_blank(),
     plot.background = element_rect(fill = "#1B2129", color = NA),
     plot.margin = margin(t = .5, r = 1.5, b = .5, l = 1.5, unit = "cm")
@@ -92,5 +92,5 @@ holders_df |>
 
 # Saving ------------------------------------------------------------------
 path <- here::here("2023", "2023_w11", "tidytuesday_2023_w11")
-ggsave(filename = glue::glue("{path}.png"), width = 9, height = 13.5, device = ragg::agg_png, dpi = 300)
+ggsave(filename = glue::glue("{path}.png"), width = 9, height = 12.5, device = ragg::agg_png, dpi = 300)
 
