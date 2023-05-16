0131aee68ad2b789de5fce1c444948ac11c0de35
AbdoulMa <issamadjid1995@gmail.com>
2023-05-16 14:31:26 +0200

    Week 20 completed.

--- 2023/2023_w20/tidytuesday_2023_w20.R
+++ 2023/2023_w20/tidytuesday_2023_w20.R
@@ -18,56 +18,52 @@ states_sf <- albersusa::usa_sf(proj = "laea")
 
 tornados_by_county <- st_join(counties_sf, tornadoes_sf, left = T)
 
-yearly_county_tornados <- tornados_by_county |> 
+county_tornados <- tornados_by_county |> 
   count(county_fips, drop = F)
 
-
 # Graphic -----------------------------------------------------------------
-yearly_county_tornados |> 
+county_tornados |> 
   mutate(n = ggplot2::cut_interval(n, n = 10)) |> 
   ggplot() + 
   geom_sf(aes(fill = n), linewidth = 0.00125, color = NA) + 
-  geom_sf(data = states_sf, fill = NA, color = "white", linewidth = 0.15) +
+  geom_sf(data = states_sf, fill = NA, color = "#111111", linewidth = 0.15) +
   labs(
-    title = "Tornados in USA", 
-    subtitle = "The number of tornados observed in  each US County",
+    title = "Tornadoes in USA", 
+    subtitle = "The number of tornadoes observed\nin  each US County (starting point) between 1951 & 2021.",
     caption = "Tidytuesday Week-20 2023<br> Abdoul ISSA BIDA <span style='font-family: \"Font Awesome 5 Brands\"'>&#xf099;</span>**@issa_madjid**<br>
-      Data from NOAA's National Weather Service Storm Prediction Center"
+      Data from NOAA's National Weather Service Storm Prediction Center."
   )+ 
   scale_fill_viridis_d(
     option = "rocket",
     direction = -1,
     begin = 0.05,
     end = 0.85, 
-    labels = c("<- Less",rep("",7),"More ->"),
+    labels = c("← Less",rep("",7),"More →"),
     guide = guide_colorsteps(
-      title = "Number of tornaods observed",
+      title = "Number of tornadoes observed",
       title.position = "top",
+      title.theme = element_text(family = "UEFA Supercup", size = rel(17.25), color = "white", face = "bold"),
+      label.theme = element_text(family = "UEFA Supercup", size = rel(15), color = "white"),
       title.hjust = 0.5,
       nrow = 1,
       barwidth = unit(10, "cm")
-      
     )
   ) + 
   theme_minimal()  +
   theme(
+    text = element_text(family = "UEFA Supercup", color = "white"),
     panel.grid = element_blank(),
     axis.title = element_blank(),
     axis.text = element_blank(),
     legend.position = "top",
-    plot.title = element_text(hjust = 1, margin = margin(t = 1, unit = "cm")),
-    plot.subtitle = element_text(hjust = 1, margin = margin(b = 1, unit = "cm")),
-    plot.caption = element_markdown(hjust = 1, margin = margin(b = 1, unit = "cm")),
-    plot.margin =  margin(0.5, unit = "cm")
+    plot.title = element_text(size = rel(4.5), face = "bold", hjust = 1, margin = margin(t = 1, b = .5, unit = "cm")),
+    plot.subtitle = element_text(size = rel(1.5), hjust = 1, margin = margin(b = 1, unit = "cm")),
+    plot.caption = element_markdown(size = rel(1.25),hjust = 1, margin = margin(t = 0.25, b = 0.5, unit = "cm")),
+    plot.background = element_rect(fill = "#171416", color = NA),
+    plot.margin =  margin(c(0.25, 0.125, 0, 0), unit = "cm")
   )
 
 # Saving ------------------------------------------------------------------
 path <- here::here("2023", "2023_w20", "tidytuesday_2023_w20")
-ggsave(filename = glue::glue("{path}.pdf"), width = 9, height = 9, device = cairo_pdf)
-
-pdftools::pdf_convert(
-  pdf = glue::glue("{path}.pdf"),
-  filenames = glue::glue("{path}.png"),
-  dpi = 320
-)
+ggsave(filename = glue::glue("{path}.png"), width = 10.5, height = 10.5, dpi = 300, device = ragg::agg_png)
 
